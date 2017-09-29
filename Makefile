4a06a3047696c67fc5a14139c5c2fe2c2e2b366b
Aaron Gorka <aaron.gorka@amaysim.com.au>
2017-09-29 14:21:13 +1000

    Add additional GoCD agent states

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.0.2
+VERSION = 0.0.3
 IMAGE_NAME ?= aarongorka/gocd-metrics:$(VERSION)
 TAG = $(VERSION)
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,14 @@ if [[ "$@" == "run" ]]; then
         TOTAL="$(echo "${FILTERED}" | wc -l)"
         IDLE="$(echo "${FILTERED}" | grep '^Idle$' | wc -l)"
         BUILDING="$(echo "${FILTERED}" | grep '^Building$' | wc -l)"
-        echo "{'@timestamp': $(date +%s), 'total': ${TOTAL}, 'idle': ${IDLE}, 'building': ${BUILDING}}"
+        CANCELLED="$(echo "${FILTERED}" | grep '^Cancelled$' | wc -l)"
+        UNKNOWN="$(echo "${FILTERED}" | grep '^Unknown$' | wc -l)"
+        echo "{'@timestamp': $(date +%s), 'total': ${TOTAL}, 'idle': ${IDLE}, 'building': ${BUILDING}, 'cancelled': ${CANCELLED}, 'unknown': ${UNKNOWN}}"
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name TotalAgents --namespace GoCD --value "${TOTAL}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name BuildingAgents --namespace GoCD --value "${BUILDING}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name IdleAgents --namespace GoCD --value "${IDLE}" --unit "Count" &
+        aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name CancelledAgents --namespace GoCD --value "${CANCELLED}" --unit "Count" &
+        aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name UnknownAgents --namespace GoCD --value "${UNKNOWN}" --unit "Count" &
         sleep 60
     done
 else
