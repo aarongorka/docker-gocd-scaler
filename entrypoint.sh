8fe2481cfe77934fdc122d8dfe942d3108360098
Aaron Gorka <aaron.gorka@amaysim.com.au>
2017-10-28 13:54:17 +1100

    Track missing agents

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,13 @@ if [[ "$@" == "run" ]]; then
         BUILDING="$(echo "${FILTERED}" | grep '^Building$' | wc -l)"
         CANCELLED="$(echo "${FILTERED}" | grep '^Cancelled$' | wc -l)"
         UNKNOWN="$(echo "${FILTERED}" | grep '^Unknown$' | wc -l)"
-        echo "{'@timestamp': $(date +%s), 'total': ${TOTAL}, 'idle': ${IDLE}, 'building': ${BUILDING}, 'cancelled': ${CANCELLED}, 'unknown': ${UNKNOWN}}"
+        MISSING="$(echo "${FILTERED}" | grep '^Missing$' | wc -l)"
+        echo "{'@timestamp': $(date +%s), 'total': ${TOTAL}, 'idle': ${IDLE}, 'building': ${BUILDING}, 'cancelled': ${CANCELLED}, 'unknown': ${UNKNOWN}, 'missing': ${MISSING}}"
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name TotalAgents --namespace GoCD --value "${TOTAL}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name BuildingAgents --namespace GoCD --value "${BUILDING}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name IdleAgents --namespace GoCD --value "${IDLE}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name CancelledAgents --namespace GoCD --value "${CANCELLED}" --unit "Count" &
-        aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name UnknownAgents --namespace GoCD --value "${UNKNOWN}" --unit "Count" &
+        aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name MissingAgents --namespace GoCD --value "${UNKNOWN}" --unit "Count" &
         sleep 50
     done
 else
