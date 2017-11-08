64f22b1e869aac04d1d1aadef8f3f9441faedccb
Aaron Gorka <aaron.gorka@amaysim.com.au>
2017-11-09 10:39:49 +1100

    Increase frequency of metrics to avoid missed timeslices

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ if [[ "$@" == "master" ]]; then
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name CancelledAgents --namespace GoCD --value "${CANCELLED}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name UnknownAgents --namespace GoCD --value "${UNKNOWN}" --unit "Count" &
         aws cloudwatch --region "${EC2_REGION}" put-metric-data --metric-name MissingAgents --namespace GoCD --value "${MISSING}" --unit "Count" &
-        sleep 50
+        sleep 40
     done
 elif [[ "$@" == "agent" ]]; then
     echo "{\"@timestamp\": $(date +%s), \"message\": \"initialising\"}"
